32b2479e6f0b12e7033c0767943ab62a4ac8378d
Jim Cannamela <jim.cannamela@outlook.com>
2023-09-05 13:40:57 -0500

    File successfully sent to S3 Bucket after changes

--- main.tf
+++ main.tf
@@ -23,6 +23,18 @@ resource "aws_s3_bucket" "jimcterraformbucket" {
   }
 }
 
+resource "aws_s3_object" "object" {
+  bucket = "jimc-tf-test-bucket"
+  key    = "sample_text_file"
+  source = "./build/libs/sample.txt"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("./build/libs/sample.txt")
+}
+
+
 
 
 
